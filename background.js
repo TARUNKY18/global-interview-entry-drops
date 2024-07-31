73f32346b8122d139f1b68b749e5f10d8db6c003
TARUNKY18 <kunduryogesh@gmail.com>
2024-07-31 14:56:27 +0530

    creating and managing notifications

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+import { fetchLocations } from "./api/fetchLocations.js";
+import { fetchOpenSlots } from "./api/fetchOpenSlots.js";
+import { createNotification } from "./lib/createNotification.js";
+
+const ALARM_JOB_NAME = "DROP_ALARM";
+
+let cachedPrefs = {};
+let firstApptTimestamp = null;
+
+chrome.runtime.onInstalled.addListener((details) => {
+	handleOnStop();
+	fetchLocations();
+});
 chrome.runtime.onMessage.addListener((data) => {
 	const { event, prefs } = data;
 	switch (event) {
@@ -12,10 +25,30 @@ chrome.runtime.onMessage.addListener((data) => {
 	}
 });
 
-function handleOnStop() {
-    console.log("Extension stopped");
-}
+chrome.notifications.onClicked.addListener(() => {
+	chrome.tabs.create({
+		url: "https://ttp.cbp.dhs.gov/schedulerui/schedule-interview/location?lang=en&vo=true&returnUrl=ttp-external&service=up",
+	});
+});
 
-function handleOnStart(prefs) {
-    console.log("Extension started with preferences:", prefs);
-}
+chrome.alarms.onAlarm.addListener(() => {
+	openSlotsJob();
+});
+
+const handleOnStop = () => {
+	setRunningStatus(false);
+	stopAlarm();
+	cachedPrefs = {};
+	firstApptTimestamp = null;
+};
+
+const handleOnStart = (prefs) => {
+	cachedPrefs = prefs;
+	chrome.storage.local.set(prefs);
+	setRunningStatus(true);
+	createAlarm();
+};
+
+const setRunningStatus = (isRunning) => {
+	chrome.storage.local.set({ isRunning });
+};
